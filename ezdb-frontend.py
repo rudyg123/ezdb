80de259a51b25bf7520580042dad6c7af32deca0
Rudy Gonzalez <gonzalru@onid.orst.edu>
2016-02-12 19:47:02 -0500

    work on table creation form and some code cleanup

--- ezdb-frontend.py
+++ ezdb-frontend.py
@@ -20,7 +20,7 @@ class Initial(npyscreen.ActionFormWithMenus):
         self.add(npyscreen.FixedText, value=" / _ \|_  / / _` || '_ \ ", editable=False)
         self.add(npyscreen.FixedText, value="|  __/ / / | (_| || |_) |", editable=False)
         self.add(npyscreen.FixedText, value=" \___|/___| \__,_||_.__/ ", editable=False)
-        self.nextrely += 3  # Extra padding
+        self.nextrely += 1  # Extra padding
 
         # Add session options and save the selected value
 
@@ -29,10 +29,6 @@ class Initial(npyscreen.ActionFormWithMenus):
                                      values = ["postgreSQL", "MySQL"],
                                      scroll_exit=True)
 
-
-        #npyscreen.notify_confirm(self.parentApp.dbtype)
-        #self.parentApp.dbtype = 'test2'
-        #npyscreen.notify_confirm(self.parentApp.dbtype)
 
         # Add a menu
         menu = self.new_menu(name="Help Menu")
@@ -47,7 +43,7 @@ class Initial(npyscreen.ActionFormWithMenus):
         self.parentApp.setNextForm("Connect_DBMS")
 
     def on_cancel(self):
-        exiting = npyscreen.notify_yes_no("Are you sure you want to quit?", "Are you sure?", editw=1)
+        exiting = npyscreen.notify_yes_no("Are you sure you want to quit?", "Are you sure?", editw=2)
         if exiting:
             self.parentApp.setNextForm(None)
         else:
@@ -81,7 +77,6 @@ class Connect_DBMS(npyscreen.ActionFormWithMenus):
         self.parentApp.username = self.add(npyscreen.TitleText, name="Username:", value=self.parentApp.username)
         self.nextrely += 1  # Move down
         self.parentApp.password = self.add(npyscreen.TitleText, name="Password:", value=self.parentApp.password)
-
 
         # Add a menu
         menu = self.new_menu(name="Help Menu")
@@ -133,10 +128,10 @@ class Database_Window(npyscreen.ActionFormWithMenus):
 
         #database button options
         self.nextrely += 1  # Move down
-        self.add(OpenDB_Button, name="Open Selected Database")
+        self.add(OpenDB_Button, name="Open Database")
 
         self.nextrely += 1  # Move down
-        self.add(npyscreen.TitleText, w_id="wNewDB_name", name="Enter New DB Name:", relx=4,
+        self.add(npyscreen.TitleText, w_id="wNewDB_name", name="New Database Name:", relx=4,
                                    begin_entry_at=22, use_two_lines=False)
 
         self.add(CreateDB_Button, name="Create")
@@ -144,10 +139,6 @@ class Database_Window(npyscreen.ActionFormWithMenus):
         self.nextrely += 1  # Move down
         self.add(DeleteDB_Button, name="Delete Database")
 
-        self.nextrely += 4  # Move down
-
-        self.add(npyscreen.BoxTitle, w_id="tableresults_box", name="Database Tables", values=self.parentApp.tablelist, max_width=50,
-                                    max_height=10, scroll_exit=True, hidden=True)
         # Add a menu
         menu = self.new_menu(name="Help Menu")
         menu.addItem("Some helpful guidance here.")
@@ -158,11 +149,11 @@ class Database_Window(npyscreen.ActionFormWithMenus):
         self.parentApp.setNextForm("MAIN")
 
 
-    #def while_waiting(self):
-    #    self.get_widget("wActiveDB").display()
+    def while_waiting(self):
+        self.get_widget("wActiveDB").display()
 
 
-class Tables_Window(npyscreen.FormWithMenus):
+class Tables_Window(npyscreen.ActionFormWithMenus):
     tabDatabases, tabTables, tabQuery, tabRawSQL, tabExport, tabAdmin, tabExit = None, None, None, None, None, None, None
 
     def create(self):
@@ -176,9 +167,25 @@ class Tables_Window(npyscreen.FormWithMenus):
 
         self.nextrely += 1  # Move down
         self.add(npyscreen.FixedText, value="Here is the TABLES window", editable=False)
-        self.nextrely += 4  # Move down
+        self.nextrely += 1  # Move down
         self.add(npyscreen.BoxTitle, w_id="tableresults_box", name="{} Tables".format(self.parentApp.active_db),
-                 values=self.parentApp.tablelist, max_width=50, max_height=10, scroll_exit=True)
+                 values=self.parentApp.tablelist, max_width=15, max_height=10, scroll_exit=True)
+
+        self.nextrely += 1  # Move down
+        self.add(OpenTable_Button, name="Open Table", relx=1)
+
+        self.nextrely += 1  # Move down
+        self.add(npyscreen.TitleText, w_id="wNewTable_name", name="New Table Name:", relx=3,
+                                  begin_entry_at=22, use_two_lines=False)
+
+        self.add(CreateTable_Button, name="Create", relx=1)
+
+        self.nextrely += 1  # Move down
+        self.add(DeleteTable_Button, name="Delete Table", relx=1)
+
+
+
+
         # Add a menu
         menu = self.new_menu(name="Help Menu")
         menu.addItem("Some helpful guidance here.")
@@ -186,7 +193,47 @@ class Tables_Window(npyscreen.FormWithMenus):
     def on_cancel(self):
         self.parentApp.setNextForm("MAIN")
 
-class Query_Window(npyscreen.FormWithMenus):
+class Table_Create_PostgreSQL_Form(npyscreen.ActionForm):
+    '''
+    field_name, field_type, field_length_or_val, field_collation, field_attrib, field_default = None
+    field_autoincrement, field_primarykey, field_unique, field_index= False
+    field_optional = True #user friendly way of saying if Null is okay for this field
+    '''
+
+
+    def create(self):
+
+        postgresql_field_type_list = ['CHAR','VARCHAR','TEXT','BIT','VARBIT','SMALLINT','INT','BIGINT','SMALLSERIAL',
+                                  'SERIAL','BIGSERIAL','NUMERIC','DOUBLE PRECISION','REAL','MONEY','BOOL',
+                                  'DATE','TIMESTAMP','TIMESTAMP WITH TIME ZONE','TIME','TIME WITH TIME ZONE','BYTEA']
+
+        postgresql_field_collat_list = ['C','POSIX','C.UTF-8','en_AG','en_AG.utf8','en_AU.utf8','en_AU.utf8','en_BW.utf8',
+                                    'en_BW.utf8','en_CA.utf8','en_CA.utf8','en_DK.utf8','en_DK.utf8','en_GB.utf8',
+                                    'en_GB.utf8','en_HK.utf8','en_HK.utf8','en_IE.utf8','en_IE.utf8','en_IN',
+                                    'en_IN.utf8','en_NG','en_NG.utf8','en_NZ.utf8','en_NZ.utf8','en_PH.utf8',
+                                    'en_PH.utf8','en_SG.utf8','en_SG.utf8','en_US.utf8','en_US.utf8','en_ZA.utf8',
+                                    'en_ZA.utf8','en_ZM','en_ZM.utf8','en_ZW.utf8','en_ZW.utf8']
+
+        postgresql_field_constraint_list = ['PRIMARY KEY','UNIQUE']
+
+        self.add(npyscreen.TitleText, w_id="wNewField_name", name="Field Name: ",
+                                   begin_entry_at=15, use_two_lines=False)
+
+        self.add(npyscreen.TitleSelectOne, w_id="wActiveDB", max_height=4, name="Type: ", value = [0,],
+                 values=postgresql_field_type_list,)
+
+class Table_Create_MySQL_Form(npyscreen.ActionForm):
+    '''
+    field_name, field_type, field_length_or_val, field_collation, field_attrib, field_default = None
+    field_autoincrement, field_primarykey, field_unique, field_index= False
+    field_optional = True #user friendly way of saying if Null is okay for this field
+    '''
+
+    def create(self):
+        self.add(npyscreen.TitleText, w_id="wNewField_name", name="Field Name:",
+                                   begin_entry_at=15, use_two_lines=False)
+
+class Query_Window(npyscreen.ActionFormWithMenus):
     tabDatabases, tabTables, tabQuery, tabRawSQL, tabExport, tabAdmin, tabExit = None, None, None, None, None, None, None
 
     def create(self):
@@ -204,7 +251,7 @@ class Query_Window(npyscreen.FormWithMenus):
         menu = self.new_menu(name="Help Menu")
         menu.addItem("Some helpful guidance here.")
 
-class RawSQL_Window(npyscreen.FormWithMenus):
+class RawSQL_Window(npyscreen.ActionFormWithMenus):
     tabDatabases, tabTables, tabQuery, tabRawSQL, tabExport, tabAdmin, tabExit = None, None, None, None, None, None, None
 
     def create(self):
@@ -218,11 +265,14 @@ class RawSQL_Window(npyscreen.FormWithMenus):
 
         self.add(npyscreen.FixedText, value="Here is the RAW SQL window", editable=False)
 
+        self.add(npyscreen.MultiLineEdit,
+                         value = """try typing here!\nMutiline text, press ^R to reformat.\n""",
+                         max_height=5, rely=9)
         # Add a menu
         menu = self.new_menu(name="Help Menu")
         menu.addItem("Some helpful guidance here.")
 
-class Export_Window(npyscreen.FormWithMenus):
+class Export_Window(npyscreen.ActionFormWithMenus):
     tabDatabases, tabTables, tabQuery, tabRawSQL, tabExport, tabAdmin, tabExit = None, None, None, None, None, None, None
 
     def create(self):
@@ -240,7 +290,7 @@ class Export_Window(npyscreen.FormWithMenus):
         menu = self.new_menu(name="Help Menu")
         menu.addItem("Some helpful guidance here.")
 
-class Admin_Window(npyscreen.FormWithMenus):
+class Admin_Window(npyscreen.ActionFormWithMenus):
     tabDatabases, tabTables, tabQuery, tabRawSQL, tabExport, tabAdmin, tabExit = None, None, None, None, None, None, None
 
     def create(self):
@@ -260,7 +310,7 @@ class Admin_Window(npyscreen.FormWithMenus):
 
 class ExitButton(npyscreen.ButtonPress):
     def whenPressed(self):
-        exiting = npyscreen.notify_yes_no("Are you sure you want to quit?", "Are you sure?", editw=1)
+        exiting = npyscreen.notify_yes_no("Are you sure you want to quit?", "Are you sure?", editw=2)
         if exiting:
             self.parent.parentApp.switchForm(None)
         else:
@@ -274,13 +324,7 @@ class OpenDB_Button(npyscreen.ButtonPress):
         self.parent.parentApp.mydb.connect_database(self.parent.parentApp.active_db)
         self.parent.parentApp.tablelist = self.parent.parentApp.mydb.list_database_tables()
 
-        self.parent.get_widget("tableresults_box").values = self.parent.parentApp.tablelist
         self.parent.parentApp.switchForm("Tables_Window")
-        #self.parent.get_widget("tableresults_box").hidden = False
-        #self.parent.get_widget("tableresults_box").display()
-
-        #npyscreen.notify_confirm("The active db is " + str(self.parent.parentApp.active_db))
-        #npyscreen.notify_confirm("The tablelist is " + str(self.parent.parentApp.tablelist))
         return self.parent.parentApp.tablelist
 
 class CreateDB_Button(npyscreen.ButtonPress):
@@ -314,7 +358,51 @@ class DeleteDB_Button(npyscreen.ButtonPress):
             npyscreen.blank_terminal() # clears the notification and just goes back to the original form
         self.parent.get_widget("wActiveDB").display()
 
+class OpenTable_Button(npyscreen.ButtonPress):
+    def whenPressed(self):
+        pass
+        '''
+        self.parent.parentApp.active_db = self.parent.get_widget("wActiveDB").get_selected_objects()[0]
+        self.parent.parentApp.mydb.connect_database(self.parent.parentApp.active_db)
+        self.parent.parentApp.tablelist = self.parent.parentApp.mydb.list_database_tables()
 
+
+        self.parent.parentApp.switchForm("Tables_Window")
+        return self.parent.parentApp.tablelist
+        '''
+class CreateTable_Button(npyscreen.ButtonPress):
+    def whenPressed(self):
+        self.newTable_name = self.parent.get_widget("wNewTable_name").value
+        create_confirm = npyscreen.notify_yes_no("Are you sure you want to create " + str(self.newTable_name)
+                                                 + "?", "Confirm Creation", editw=1)
+        if create_confirm:
+            self.parent.parentApp.table_name = self.parent.parentApp.mydb.create_database(self.newTable_name)
+
+            if self.parent.parentApp.dbtype == 0:
+                self.parent.parentApp.switchForm("Table_Create_PostgreSQL_Form")
+            elif self.parent.parentApp.dbtype == 1:
+                self.parent.parentApp.switchForm("Table_Create_MySQL_Form")
+
+        else:
+            npyscreen.blank_terminal() # clears the notification and just goes back to the original form
+
+
+class DeleteTable_Button(npyscreen.ButtonPress):
+    def whenPressed(self):
+        pass
+        '''
+        self.parent.parentApp.active_db = self.parent.get_widget("wActiveDB").get_selected_objects()[0]
+        delete_confirm = npyscreen.notify_yes_no("Are you sure you want to delete " + str(self.parent.parentApp.active_db)
+                                                 + "?", "Confirm Deletion", editw=1)
+        if delete_confirm:
+            servermsg = self.parent.parentApp.mydb.delete_database(self.parent.parentApp.active_db)
+            npyscreen.notify_confirm(servermsg)
+            self.parent.get_widget("wActiveDB").display()
+            return
+
+        else:
+            npyscreen.blank_terminal() # clears the notification and just goes back to the original form
+        '''
 class Tab_DatabaseButton(npyscreen.ButtonPress):
     def whenPressed(self):
         self.parent.parentApp.switchForm("Database_Window")
@@ -354,10 +442,15 @@ class App(npyscreen.NPSAppManaged):
     port = None
     username = None
     password = None
-
     mydb = None
     active_db = None
     tablelist = None
+
+    #table creation global variables
+    field_name, field_type, field_length_or_val, field_collation, field_attrib, field_default = None, None, None, None, None, None
+
+    field_autoincrement, field_primarykey, field_unique, field_index= False, False, False, False
+    field_optional = True #user friendly way of saying if Null is okay for this field
 
     def onStart(self):
 
@@ -370,6 +463,8 @@ class App(npyscreen.NPSAppManaged):
         self.addFormClass("RawSQL_Window", RawSQL_Window, name = "ezdb Raw SQL Page")
         self.addFormClass("Export_Window", Export_Window, name = "ezdb Export Page")
         self.addFormClass("Admin_Window", Admin_Window, name = "ezdb Admin Page")
+        self.addFormClass("Table_Create_PostgreSQL_Form", Table_Create_PostgreSQL_Form, name = "ezdb Create Table")
+        self.addFormClass("Table_Create_MySQL_Form", Table_Create_MySQL_Form, name = "ezdb Create Table")
 
 if __name__ == "__main__":
     # Start an NPSAppManaged application mainloop
